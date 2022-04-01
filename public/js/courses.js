3f17d37f7c7b96cfd428ba15750e6784453b0189
tim <tjhickey@brandeis.edu>
2022-04-01 17:58:18 -0400

    minor changes

--- public/js/courses.js
+++ public/js/courses.js
@@ -1,12 +1,11 @@
 
 
 const courses = require('../data/courses20-21.json');
-console.dir(courses[2000])
+
 function byInstructorEmail(inst){
     let cs = courses.filter(course=>(course.instructor[2].includes(inst)))
     return cs    
 }
 
-console.dir(byInstructorEmail("tjhickey"))
 
 module.exports = {courses,byInstructorEmail};
