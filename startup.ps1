b2f4f81ce21230ba8fb523ac3a4598989f5052ed
tim <tjhickey@brandeis.edu>
2022-04-26 11:20:24 -0400

    added startup.ps1 for windows powershell script

---
+++ startup.ps1
@@ -0,0 +1,2 @@
+$Env.mongodb_URI = 'mongodb://localhost:27017/cs103a_L36'
+npm start
--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export mongodb_URI='mongodb+srv://cs_sj:BrandeisSpr22@cluster0.kgugl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+export mongodb_URI='mongodb://localhost:27017/cs103a_L36'
 echo "connecting to $mongodb_URI"
 nodemon
