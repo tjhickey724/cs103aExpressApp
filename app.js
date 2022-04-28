09337a7626ab4446beb215330eb6a3fc089b1e85
tim <tjhickey@brandeis.edu>
2022-04-28 15:27:57 -0400

    cleaned up code

--- app.js
+++ app.js
@@ -20,9 +20,8 @@ var MongoDBStore = require('connect-mongodb-session')(session);
 // *********************************************************** //
 //  Loading models
 // *********************************************************** //
-const ToDoItem = require("./models/ToDoItem")
-const Course = require('./models/Course')
-const Schedule = require('./models/Schedule')
+
+const Name = require('./models/Name')
 
 // *********************************************************** //
 //  Loading JSON datasets
@@ -137,34 +136,20 @@ app.get("/", (req, res, next) => {
   res.render("exam13");
 });
 
-app.get('/routing', (req,res,next) =>{
+app.get('/exam13a-routing', (req,res,next) =>{
   res.render('exam13a-form')
 })
 
-app.post('/routing', (req,res,next) => {
+app.post('/exam13a-routing', (req,res,next) => {
   // add the routing code to calculate the BMI
   // and send the appropriate info back to 
   // exam13a-result
   res.send('under construction')
 })
 
-// Here is where we load in the names data,
-// but you don't have to do this as I have preloaded it
-const Name = require('./models/Name')
-const names = require('./public/data/names.json')
-app.get('/loadNames',
-  async (req,res,next) => {
-    try {
-      await Name.deleteMany({})
-      await Name.insertMany(names)
-      const n = await Name.find({name:'Timothy',sex:'M'})
-      res.json(n)
-    } catch (error) {
-      next(error)
-    }
-})
 
-app.get('/mongodb', (req,res,next) =>{
+
+app.get('/exam13b-mongodb', (req,res,next) =>{
   res.render('exam13b-mongodb')
 })
 
@@ -180,7 +165,7 @@ const popularNames = [
  // put the aggregation pipeline here!
 ]
 
-app.get('/aggregation', 
+app.get('/exam13c-aggregation', 
   async (req,res,next) =>{
     try {
       res.locals.people =
@@ -198,525 +183,22 @@ app.get("/about", (req, res, next) => {
   res.render("about");
 });
 
-app.get("/demo/:subject",
- async (req,res,next) => {
-  try{
-    const theCourses = await Course.find({subject:req.params.subject})
-    res.json(theCourses)
-  } catch (e){
-    next(e);
-  }
-})
 
-app.get("/demo",
- async (req,res,next) => {
-  try{
-    const theCourses = 
-        await Course.find(
-          {subject:'COSI',
-          independent_study:true,
-          enrolled:{$gt:100},
-        })
-    res.json(theCourses)
-  } catch (e){
-    next(e);
-  }
-})
-
-
-/*
-    ToDoList routes
-*/
-app.get('/todo',
-  isLoggedIn,   // redirect to /login if user is not logged in
-  async (req,res,next) => {
-    try{
-      let userId = res.locals.user._id;  // get the user's id
-      let items = await ToDoItem.find({userId:userId}); // lookup the user's todo items
-      res.locals.items = items;  //make the items available in the view
-      res.render("toDo");  // render to the toDo page
-    } catch (e){
-      next(e);
-    }
-  }
-  )
-
-  app.post('/todo/add',
-  isLoggedIn,
-  async (req,res,next) => {
-    try{
-      const {title,description} = req.body; // get title and description from the body
-      const userId = res.locals.user._id; // get the user's id
-      const createdAt = new Date(); // get the current date/time
-      let data = {title, description, userId, createdAt,} // create the data object
-      let item = new ToDoItem(data) // create the database object (and test the types are correct)
-      await item.save() // save the todo item in the database
-      res.redirect('/todo')  // go back to the todo page
-    } catch (e){
-      next(e);
-    }
-  }
-  )
-
-  app.get("/todo/delete/:itemId",
-    isLoggedIn,
-    async (req,res,next) => {
-      try{
-        const itemId=req.params.itemId; // get the id of the item to delete
-        await ToDoItem.deleteOne({_id:itemId}) // remove that item from the database
-        res.redirect('/todo') // go back to the todo page
-      } catch (e){
-        next(e);
-      }
-    }
-  )
-
-  app.get("/todo/completed/:value/:itemId",
-  isLoggedIn,
-  async (req,res,next) => {
-    try{
-      const itemId=req.params.itemId; // get the id of the item to delete
-      const completed = req.params.value=='true';
-      await ToDoItem.findByIdAndUpdate(itemId,{completed}) // remove that item from the database
-      res.redirect('/todo') // go back to the todo page
-    } catch (e){
-      next(e);
-    }
-  }
-)
-
-/* ************************
-  Functions needed for the course finder routes
-   ************************ */
-
-function getNum(coursenum){
-  // separate out a coursenum 103A into 
-  // a num: 103 and a suffix: A
-  i=0;
-  while (i<coursenum.length && '0'<=coursenum[i] && coursenum[i]<='9'){
-    i=i+1;
-  }
-  return coursenum.slice(0,i);
-}
-
-
-function times2str(times){
-  // convert a course.times object into a list of strings
-  // e.g ["Lecture:Mon,Wed 10:00-10:50","Recitation: Thu 5:00-6:30"]
-  if (!times || times.length==0){
-    return ["not scheduled"]
-  } else {
-    return times.map(x => time2str(x))
-  }
-  
-}
-function min2HourMin(m){
-  // converts minutes since midnight into a time string, e.g.
-  // 605 ==> "10:05"  as 10:00 is 60*10=600 minutes after midnight
-  const hour = Math.floor(m/60);
-  const min = m%60;
-  if (min<10){
-    return `${hour}:0${min}`;
-  }else{
-    return `${hour}:${min}`;
-  }
-}
-
-function time2str(time){
-  // creates a Times string for a lecture or recitation, e.g. 
-  //     "Recitation: Thu 5:00-6:30"
-  const start = time.start
-  const end = time.end
-  const days = time.days
-  const meetingType = time['type'] || "Lecture"
-  const location = time['building'] || ""
-
-  return `${meetingType}: ${days.join(",")}: ${min2HourMin(start)}-${min2HourMin(end)} ${location}`
-}
-
-
-
-/* ************************
-  Loading (or reloading) the data into a collection
-   ************************ */
-// this route loads in the courses into the Course collection
-// or updates the courses if it is not a new collection
-
-app.get('/upsertDB',
-  async (req,res,next) => {
-    await Course.deleteMany({})
-    for (course of courses){ 
-      const {subject,coursenum,section,term}=course;
-      const num = getNum(coursenum);
-      course.num=num
-      course.suffix = coursenum.slice(num.length)
-      await Course.findOneAndUpdate({subject,coursenum,section,term},course,{upsert:true})
-    }
-    const num = await Course.find({}).countDocuments();
-    res.send("data uploaded: "+num)
-  }
-)
-
-
-app.post('/courses/bySubject',
-  // show list of courses in a given subject
-  async (req,res,next) => {
-    const {subject} = req.body;
-    const courses = await Course.find({subject:subject,independent_study:false}).sort({term:1,num:1,section:1})
-    
-    res.locals.courses = courses
-    res.locals.times2str = times2str
-    //res.json(courses)
-    res.render('courselist')
-  }
-)
-app.get('/courses/bySubject/:subject',
-  // show list of courses in a given subject
-  async (req,res,next) => {
-    const {subject} = req.params;
-    const courses = await Course.find({subject:subject,independent_study:false}).sort({term:1,num:1,section:1})
-    
-    res.locals.courses = courses
-    res.locals.times2str = times2str
-    //res.json(courses)
-    res.render('courselist')
-  }
-)
-
-app.get('/courses/bySubject/:subject/:coursenum',
-  // show list of courses in a given subject 
-  async (req,res,next) => {
-    const {subject,coursenum} = req.params;
-    const courses = await Course.find({subject,coursenum,independent_study:false}).sort({term:1,num:1,section:1})
-    
-    res.locals.courses = courses
-    res.locals.times2str = times2str
-    //res.json(courses)
-    res.render('courselist')
-  }
-)
-
-app.get('/courses/bySubject/:subject/:coursenum/:section',
-  // show list of courses in a given subject
-  async (req,res,next) => {
-    const {subject,coursenum,section} = req.params;
-    const courses = 
-      await Course.find({subject,coursenum,section,independent_study:false}).sort({term:1,num:1,section:1})
-    
-    res.locals.courses = courses
-    res.locals.times2str = times2str
-    //res.json(courses)
-    res.render('courselist')
-  }
-)
-
-app.get('/courses/show/:courseId',
-  // show all info about a course given its courseid
-  async (req,res,next) => {
-    const {courseId} = req.params;
-    const course = await Course.findOne({_id:courseId})
-    res.locals.course = course
-    res.locals.times2str = times2str
-    //res.json(course)
-    res.render('course')
-  }
-)
-
-app.get('/courses/byInst/:email',
-  // show a list of all courses taught by a given faculty
-  async (req,res,next) => {
-    let email = req.params.email;
-    email = (email.indexOf('@')>0?email:email+"@brandeis.edu")
-    const courses = 
-       await Course
-         .find({instructor:email,enrolled:{$gt:0}})
-         .sort({term:1,enrolled:-1,coursenum:1})
-    //res.json(courses)
-    res.locals.courses = courses
-    res.locals.times2str = times2str
-    res.render('courselist')
-  } 
-)
-
-app.get('/courses/byName/:name',
-  async (req,res,next) => {
-    let name = req.params.name;
-    const courses = 
-       await Course
-         .find({name,enrolled:{$gt:0}})
-         .sort({term:1,enrolled:-1})
-    //res.json(courses)
-    res.locals.courses = courses
-    res.locals.times2str = times2str
-    res.render('courselist')
-  } 
-)
-
-app.post('/courses/byInst',
-  // show courses taught by a faculty send from a form
-  async (req,res,next) => {
-    try {
-          const email = req.body.email+"@brandeis.edu";
-          const courses = 
-            await Course
-                    .find({instructor:email,independent_study:false})
-                    .sort({term:1,num:1,section:1})
-          //res.json(courses)
-          res.locals.courses = courses
-          res.locals.times2str = times2str
-          res.render('courselist')
-    } catch(error){
-      next(error)
-    }
-  }
-)
-
-/*
-  aggregation example ...
-  here we create an aggregation pipeline using the
-  mongo compass aggregation tool
-  and then use it to find the total number of students
-  enrolled in each subject
-*/
-const demo4stages =
-
-    [
-      // first we add a new field, email, which is the third element of the instructor value
-      { $addFields: {  email: { $arrayElemAt: [ '$instructor', 2]}}},
-
-      // then we filter out courses with <8 students
-      {$match: {  enrolled: {$gte:8}}},
-
-      // then we group by email and find average enrollment
-      {$group: {
-            _id: '$email',
-            courseCount: {  $avg: '$enrolled'}
-        }},
-
-      // then we sort by courseCount, decreasing
-      {$sort: { 'courseCount': -1}}
-    ]
-
-
-const pivotDemo = 
-      [
-        { // then we filter out courses with <8 students
-          $match: {
-            'enrolled': {
-              '$gte': 8
-            }
-          }
-        }, {// then we group by email and find various enrollment stats
-          $group: {
-            '_id': '$instructor', 
-            'courseCount': {
-              '$sum': 1
-            },
-            'avgClassSize': {
-              '$avg': '$enrolled'
-            },
-            'maxClassSize': {
-              '$max': '$enrolled'
-            },
-            'minClassSize': {
-              '$min': '$enrolled'
-            },
-            'totalEnrollment': {
-              '$sum': '$enrolled'
-            },
-            'classes':{
-              '$push': {s:'$subject',c:'$coursenum',z:'$section',t:'$term',e:'$enrolled',n:'$name'}
-            }
-          }
-        }, // then we filter for faculty with at least 300 students total
-            {$match: {
-              'totalEnrollment': {
-                '$gte': 300
-              }
-            }
-          }, { // then we sort by courseCount, decreasing
-          $sort: {
-            'totalEnrollment': -1
-          }
-        }
-      ]
-
-const deptTeachingLoads = (dept) => 
-      [
-        { // then we filter out courses with <8 students
-          $match: {
-            'subject': dept,
-            'enrolled':{$gt:0},
-          }
-        }, {// then we group by email and find various enrollment stats
-          $group: {
-            '_id': '$instructor', 
-            'courseCount': {
-              '$sum': 1
-            },
-            'avgClassSize': {
-              '$avg': '$enrolled'
-            },
-            'maxClassSize': {
-              '$max': '$enrolled'
-            },
-            'minClassSize': {
-              '$min': '$enrolled'
-            },
-            'totalEnrollment': {
-              '$sum': '$enrolled'
-            },
-          }
-        }, { // then we sort by courseCount, decreasing
-          $sort: {
-            'totalEnrollment': -1
-          }
-        }
-      ]
-
-
-app.get('/demo4stages',
-  async (req,res,next) => {
-   try {
-    const enrollments = 
-      await Course.aggregate(
-        demo4stages
-      )
-    res.json(enrollments)
-   } catch(error) {
-     next(error)
-   }
-  })
-
-  app.get('/pivot/onInstructor',
-  async (req,res,next) => {
-   try {
-    const enrollments = 
-      await Course.aggregate(
-        pivotDemo
-      )
-    res.json(enrollments)
-   } catch(error) {
-     next(error)
-   }
-  })
-
-
-  app.get('/deptTeachingLoads/:subject',
-  async (req,res,next) => {
-   try {
-    const enrollments = 
-      await Course.aggregate(
-        deptTeachingLoads(req.params.subject)
-      )
-    res.locals.subject = req.params.subject
-    res.locals.enrollments = enrollments
-    res.render('enrollmentsBySubject')
-   } catch(error) {
-     next(error)
-   }
-  })
-
-  const smallClassDepts = 
-  [
-    {
-      '$match': {
-        'enrolled': {
-          '$lt': 10
-        }
-      }
-    }, {
-      '$group': {
-        '_id': '$subject', 
-        'numStudents': {
-          '$sum': '$enrolled'
-        }
-      }
-    }, {
-      '$sort': {
-        'numStudents': -1
-      }
-    }
-  ]
-
-
-app.get('/smallclasses', 
-  async (req,res,next) => {
-    const data = await Course.aggregate(smallClassDepts)
-    res.json(data)
-
-})
-
-app.get('/bigclasses/:size',
-  async (req,res,next) => {
-   try {
-    const size = parseInt(req.params.size)
-    const enrollments = 
-      await Course.aggregate(
-        [
-          {$match:{'enrolled':{$gt:size}}},
-          {$sort:{'enrolled':1}}
-        ]
-      )
-    res.json(enrollments)
-   } catch(error) {
-     next(error)
-   }
-  })
-
-
-app.use(isLoggedIn)
-
-app.get('/addCourse/:courseId',
-  // add a course to the user's schedule
-  async (req,res,next) => {
-    try {
-      const courseId = req.params.courseId
-      const userId = res.locals.user._id
-      // check to make sure it's not already loaded
-      const lookup = await Schedule.find({courseId,userId})
-      if (lookup.length==0){
-        const schedule = new Schedule({courseId,userId})
-        await schedule.save()
-      }
-      res.redirect('/schedule/show')
-    } catch(e){
-      next(e)
-    }
-  })
-
-app.get('/schedule/show',
-  // show the current user's schedule
-  async (req,res,next) => {
-    try{
-      const userId = res.locals.user._id;
-      const courseIds = 
-         (await Schedule.find({userId}))
-                        .sort(x => x.term)
-                        .map(x => x.courseId)
-      res.locals.courses = await Course.find({_id:{$in: courseIds}})
-      res.render('schedule')
-    } catch(e){
-      next(e)
-    }
-  }
-)
-
-app.get('/schedule/remove/:courseId',
-  // remove a course from the user's schedule
-  async (req,res,next) => {
-    try {
-      await Schedule.remove(
-                {userId:res.locals.user._id,
-                 courseId:req.params.courseId})
-      res.redirect('/schedule/show')
-
-    } catch(e){
-      next(e)
-    }
-  }
-)
-
+// Here is where we loaded in the names data,
+// but you don't have to do this as I have preloaded it
+// 
+// const names = require('./public/data/names.json')
+// app.get('/loadNames',
+//   async (req,res,next) => {
+//     try {
+//       await Name.deleteMany({})
+//       await Name.insertMany(names)
+//       const n = await Name.find({name:'Timothy',sex:'M'})
+//       res.json(n)
+//     } catch (error) {
+//       next(error)
+//     }
+//})
 
 // here we catch 404 errors and forward to error handler
 app.use(function(req, res, next) {
