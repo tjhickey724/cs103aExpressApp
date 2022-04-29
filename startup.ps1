4aeb33f261043dec8e2724447561951713f78cce
tim <tjhickey@brandeis.edu>
2022-04-29 07:26:11 -0400

    fixed typo in startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,3 +1,2 @@
-$Env.mongodb_URI='mongodb+srv://cs_sj:BrandeisSpr22@cluster0.kgugl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
+$Env:mongodb_URI='mongodb+srv://cs_sj:BrandeisSpr22@cluster0.kgugl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 npm start
